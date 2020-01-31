a7d18cc59b387c1a97e4215e92dd4fcd95808920
pip-peter <peterkir5693@outlook.com>
2020-01-31 18:51:43 +0300

    more commit on flasky-blog

--- basic.py
+++ basic.py
@@ -10,6 +10,12 @@ def index():
 @app.route("/information")
 def info():
     return "<h1> puppies are cute! </h1>"
+    
+#dynamic routing
+@app.route("/puppy/<name>")
+def puppy(name):
+    return "<h1> this is a page for {}</h1>".format(name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
