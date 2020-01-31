b44d5a497b186a21afcc96939c9ff45826a10258
pip-peter <peterkir5693@outlook.com>
2020-01-31 18:36:35 +0300

    first route commit

---
+++ basic.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return "<h1>Hello world!</h1>"
+
+@app.route("/information")
+def info():
+    return "<h1> puppies are cute! </h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
